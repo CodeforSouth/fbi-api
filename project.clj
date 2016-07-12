012be40b86edc07e9e9a97a1372a5629aea1be47
Leo Ribeiro <leordev@gmail.com>
2016-07-12 00:27:44 -0400

    project restructure and heroku deploy

--- project.clj
+++ project.clj
@@ -12,7 +12,6 @@
                  [compojure "1.5.1"]
                  [org.clojure/java.jdbc "0.6.2-alpha1"]
                  [mysql/mysql-connector-java "5.1.6"]]
-  :target-path "target/%s"
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
