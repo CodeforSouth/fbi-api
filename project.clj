e412f6fbeccae1769dd8cb52ef4293a1f4fc9534
Joel Quiles <quilesbaker@gmail.com>
2016-08-10 17:05:42 -0400

    making project more standard

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [javax.servlet/servlet-api "2.5"]
-                 [http-kit "2.2.0"]
+                 [http-kit "2.2.0-beta1"]
                  [ring/ring-devel "1.5.0"]
                  [cheshire "5.6.3"]
                  [environ "1.1.0"]
@@ -14,9 +14,9 @@
                  [clojure-csv/clojure-csv "2.0.1"]
                  [org.clojure/tools.logging "0.3.1"]
                  [yesql "0.5.3"]
-                 [org.clojure/java.jdbc "0.6.1"]
-                 [mysql/mysql-connector-java "6.0.3"]]
+                 [org.clojure/java.jdbc "0.6.2-alpha1"]
+                 [mysql/mysql-connector-java "5.1.6"]]
   :min-lein-version "2.0.0"
   :uberjar-name "restaurant-inspections-api.jar"
-  :main ^:skip-aot restaurant_inspections_api.core
+  :main ^:skip-aot restaurant-inspections-api.core
   :profiles {:uberjar {:aot :all}})
