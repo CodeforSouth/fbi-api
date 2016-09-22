3fe9bb807279380b889edf773caa2daf3805867d
Joel Quiles <quilesbaker@gmail.com>
2016-09-21 20:38:14 -0400

    checkpoint. no dates so far

--- src/restaurant_inspections_api/routes.clj
+++ src/restaurant_inspections_api/routes.clj
@@ -4,24 +4,21 @@
 
 (defroutes all-routes
            (GET "/" [] (srv/home))
-           (GET "/inspection/:id" [id] (srv/get-details id))
-           (GET "/list-counties" [] (srv/get-dist-counties))
-           (GET ["/location/:zips" :zips #"[^/]+"]
+           (GET "/inspections/:id" [id] (srv/get-details id))
+           (GET "/counties" [] (srv/get-dist-counties))
+           (GET ["/locations/:zips"]
                 [startDate endDate zips]
                 (srv/location startDate endDate zips))
-           (GET ["/name/:name"
-                 :name #"[^/]+"]
+           (GET ["/restaurants/:name"]
                 [startDate endDate name]
                 (srv/get-name startDate endDate name))
-           (GET ["/name/:name/:zips"
-                 :name #"[^/]+"
-                 :zips #"[^/]+"]
+           (GET ["/businesses/:name/:zips"]
                 [startDate endDate name zips]
                 (srv/get-name startDate endDate name zips))
-           (GET "/district/:id"
+           (GET "/districts/:id"
                 [startDate endDate id]
                 (srv/district id startDate endDate))
-           (GET "/county/:id"
+           (GET "/counties/:id"
                 [startDate endDate id]
                 (srv/county id startDate endDate)))
 
