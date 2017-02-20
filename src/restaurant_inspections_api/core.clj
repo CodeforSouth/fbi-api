5cdf6002661d266313065d79c48311288ade6c67
Joel Quiles <quilesbaker@gmail.com>
2017-02-19 23:54:58 -0500

    checkpoint generating dummy content

--- src/restaurant_inspections_api/core.clj
+++ src/restaurant_inspections_api/core.clj
@@ -4,11 +4,15 @@
             [compojure.middleware :as middleware]
             [compojure.handler :refer [site]]
             [taoensso.timbre :as log]
+            [schema.core :as s]
+            [compojure.api.sweet :as sweet]
             ;; internal
             [restaurant-inspections-api.constants :as const]
             [restaurant-inspections-api.cors :refer [all-cors]]
             [restaurant-inspections-api.routes :refer [all-routes]]
+            ;; [restaurant-inspections-api.docs :as docs]
             [restaurant-inspections-api.cron.core :refer [load-api-data]])
+
   (:use [clojure.tools.nrepl.server :only (start-server stop-server)])
   (:gen-class))
 
@@ -27,11 +31,39 @@
         (log/error e "GENERIC ERROR bubbled up. replace specific expection here and add to the catch statements.")
         {:status 500 :body "INTERNAL SERVER ERROR"}))))
 
-(def api (-> all-routes
+(s/defschema User {:id s/Str,
+                   :name s/Str
+                   :address {:street s/Str
+                             :city (s/enum :tre :hki)}})
+
+(def api (->> all-routes
               middleware/wrap-canonical-redirect
               wrap-exception-handling
               site
-              all-cors))
+              all-cors
+              (sweet/api {:swagger
+                          {:ui "/api-docs"
+                           :spec "/swagger.json"
+                           :data {:info {:title "Sausages",
+                                         :version "1.0.0",
+                                         :description "Sausage description",
+                                         :termsOfService "http://helloreverb.com/terms/",
+                                         :contact {:name "My API Team",
+                                                   :email "foo@example.com",
+                                                   :url "http://www.metosin.fi"},
+                                         :license {:name "Eclipse Public License",
+                                                   :url "http://www.eclipse.org/legal/epl-v10.html"}},
+                                  :produces ["application/json"],
+                                  :consumes ["application/json"],
+                                  :tags [{:name "user", :description "User stuff"}],
+                                  :paths {"/api/ping" {:get {:responses {"200" {:schema User
+                                                                                :description "Found it!"}}}}},
+                                  :definitions {"User" {:type "object",
+                                                        :properties {:id {:type "string"},
+                                                                     :name {:type "string"},
+                                                                     :address {:ref "#/definitions/UserAddress"}},
+                                                        :additionalProperties false,
+                                                        :required (:id :name :address)}}}}})))
 
 (def app
   (if const/production?
